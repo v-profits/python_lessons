a1338d8affda136a9274d20f7e2625db41460f43
profits <v-profit@yandex.ru>
2021-06-18 12:27:06 +0400

    урок 1.7

---
+++ 1.7.py
@@ -0,0 +1,26 @@
+# test = None
+# test = 123
+# print(test)
+
+# x=1
+# y=5
+# x,y=1,5
+# print(x,y)
+
+# bool
+# my_true=True
+# my_false=False
+# print(type(my_false))
+# print(type(my_true))
+
+# str() int() float() bool()
+x=2.5
+print(x,type(x))
+x=int(x)
+print(x,type(x))
+x=2.5
+x=str(x)
+print(x,type(x))
+x=''
+x=bool(x)
+print(x,type(x))
